eb4266ea7a0c5d38a5d17c05f5d87be047abc975
Monsalves <f.monsalves03@ufromail.cl>
2023-04-24 16:45:32 -0400

    analisis y codigo

---
+++ src/main/java/Gato.java
@@ -0,0 +1,18 @@
+public class Gato {
+    String Nombre;
+    int edad;
+    String color_de_pelo;
+    String raza;
+
+    public void comer(){
+    }
+    public void dormir(){
+    }
+    public void cazar(){
+    }
+    public void jugar(){
+    }
+    public void caminar(){
+    }
+}
+
